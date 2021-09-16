a256766767c407135b36732f510740801d85f557
satangtec <76553480+satangtec@users.noreply.github.com>
2021-09-16 09:27:10 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Suspense, useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
+import Model from './Model'
+
+export default function Viewer() {
+  const ref = useRef()
+  return (
+    <Canvas dpr={[1, 2]} camera={{ fov: 50 }}>
+      <Suspense fallback={null}>
+        <Stage controls={ref} contactShadowBlur={6}>
+          <Model />
+        </Stage>
+      </Suspense>
+      <OrbitControls ref={ref} autoRotate makeDefault />
+    </Canvas>
+  )
+}
